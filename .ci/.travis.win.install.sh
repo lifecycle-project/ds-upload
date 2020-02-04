1c59ad725877f6d1ba4f9c97b13ac3ad6dd0694e
sidohaakma <sido@haakma.org>
2020-02-04 21:17:56 +0100

    fix: rtools removed from installation procedure

--- .ci/.travis.win.install.sh
+++ .ci/.travis.win.install.sh
@@ -6,8 +6,6 @@ choco install -y r.project &&
 echo 'options(repos = "https://cloud.r-project.org")' > ~/.Rprofile.site &&
 export R_PROFILE=~/.Rprofile.site &&
 
-curl -Ls https://cran.r-project.org/bin/windows/Rtools/Rtools35.exe && 
-Rtools.exe /VERYSILENT /SP- /NORESTART &&
 Rscript.exe -e 'sessionInfo()' &&
 Rscript.exe -e 'install.packages(c("devtools"));if (!all("devtools" %in% installed.packages())) { q(status = 1, save = "no")}' &&
 Rscript.exe -e 'deps <- devtools::dev_package_deps(dependencies = NA);devtools::install_deps(dependencies = TRUE)' &&
