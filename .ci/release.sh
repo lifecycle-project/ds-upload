6a728c78694895f023e8a3eced32abd6d4346748
sidohaakma <sido@haakma.org>
2020-11-11 22:05:54 +0100

    chore: added release cycle to the ci

--- .ci/release.sh
+++ .ci/release.sh
@@ -1,10 +1,14 @@
-Rscript -e "usethis::use_version('dev')"
+Rscript -e "usethis::use_version('${RELEASE_SCOPE}')"
 TAG=$(grep Version DESCRIPTION | head -n1 | cut -d':' -f2)
 PACKAGE=$(grep Package DESCRIPTION | head -n1 | cut -d':' -f2)
-git commit -a -m 'Increment version number'
+git commit -a -m 'Created release: ${TAG}'
 echo "Releasing ${PACKAGE} v${TAG}"
 R CMD build .
 Rscript -e 'devtools::check_built(path = "./${PACKAGE}_${TAG}.tar.gz", remote=TRUE, force_suggests = TRUE)'
 set +x; curl -v --user '${NEXUS_USER}:${NEXUS_PASS}' --upload-file ${PACKAGE}_${TAG}.tar.gz ${REGISTRY}/src/contrib/${PACKAGE}_${TAG}.tar.gz
 git tag v${TAG}
+git push --tags origin master
+echo "Creating new development version"
+Rscript -e "usethis::use_version('dev')"
+git commit -a -m 'Increment dev-version number'
 git push --tags origin master
