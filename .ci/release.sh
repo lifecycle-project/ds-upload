f8cf45669461556042289e1912b78c7707e84f17
sidohaakma <sido@haakma.org>
2020-11-25 09:33:24 +0100

    chore: updated pushing tags and upload new version

--- .ci/release.sh
+++ .ci/release.sh
@@ -4,14 +4,14 @@ git checkout master
 Rscript -e "usethis::use_version('${RELEASE_SCOPE}')"
 TAG=$(grep Version DESCRIPTION | head -n1 | cut -d':' -f2)
 PACKAGE=$(grep Package DESCRIPTION | head -n1 | cut -d':' -f2)
-git commit -a -m 'Created release: ${TAG}'
+git commit -a -m "Created release: ${TAG}"
 echo "Releasing ${PACKAGE} ${TAG}"
 R CMD build .
-Rscript -e 'devtools::check_built(path = "./${PACKAGE}_${TAG}.tar.gz", remote=TRUE, force_suggests = TRUE)'
-set +x; curl -v --user '${NEXUS_USER}:${NEXUS_PASS}' --upload-file ${PACKAGE}_${TAG}.tar.gz ${REGISTRY}/src/contrib/${PACKAGE}_${TAG}.tar.gz
-git tag v${TAG}
-git push --tags origin master
+Rscript -e "devtools::check_built(path = './${PACKAGE}_${TAG}.tar.gz', remote=TRUE, force_suggests = TRUE)"
+set +x; curl -v --user "${NEXUS_USER}:${NEXUS_PASS}" --upload-file "${PACKAGE}_${TAG}".tar.gz "${REGISTRY}"/src/contrib/"${PACKAGE}_${TAG}".tar.gz
+git tag "${TAG}"
+git push origin "${TAG}"
 echo "Creating new development version"
 Rscript -e "usethis::use_version('dev')"
-git commit -a -m 'Increment dev-version number'
-git push --tags origin master
+git commit -a -m '[ci skip]: Increment dev-version number'
+git push origin master
