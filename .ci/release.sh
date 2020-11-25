2d33bb776c39bc77bbcb4ab3b4a635448a8444b6
sidohaakma <sido@haakma.org>
2020-11-25 09:11:03 +0100

    chore: updated vignettes for CRAN

--- .ci/release.sh
+++ .ci/release.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+git checkout master
 Rscript -e "usethis::use_version('${RELEASE_SCOPE}')"
 TAG=$(grep Version DESCRIPTION | head -n1 | cut -d':' -f2)
 PACKAGE=$(grep Package DESCRIPTION | head -n1 | cut -d':' -f2)
 git commit -a -m 'Created release: ${TAG}'
-echo "Releasing ${PACKAGE} v${TAG}"
+echo "Releasing ${PACKAGE} ${TAG}"
 R CMD build .
 Rscript -e 'devtools::check_built(path = "./${PACKAGE}_${TAG}.tar.gz", remote=TRUE, force_suggests = TRUE)'
 set +x; curl -v --user '${NEXUS_USER}:${NEXUS_PASS}' --upload-file ${PACKAGE}_${TAG}.tar.gz ${REGISTRY}/src/contrib/${PACKAGE}_${TAG}.tar.gz
