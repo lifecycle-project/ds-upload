f7bbcba65d8196b54a363f74dd53f6d76368a5fe
sidohaakma <sido@haakma.org>
2021-01-08 14:54:58 +0100

    feat: add covariates input to the methyl upload

--- R/upload_methyl.R
+++ R/upload_methyl.R
@@ -6,9 +6,9 @@ ds_upload.globals <- new.env()
 #' @param upload do we need to upload the DataSHIELD backend
 #' @param dict_name name of the dictionary located on Github usually something like this: diabetes/test_vars_01
 #' @param action action to be performed, can be 'populate' or 'all'
-#' @param data_input_path path to the methylation data
+#' @param methyl_data_input_path path to the methylation data
+#' @param covariate_data_input_path path to the covariate data to measure the age
 #' @param data_version version of the raw data
-#' @param age_when_measured age of the child when the measurement took place (in months)
 #' @param database_name is the name of the data backend of DataSHIELD, default = opal_data
 #'
 #' @importFrom readr read_csv write_csv
@@ -18,13 +18,13 @@ ds_upload.globals <- new.env()
 #' \dontrun{
 #' du.upload.methyl.clocks(
 #'   dict_name = "methylation_clocks",
-#'   data_input_path = "https://github.com/isglobal-brge/methylclock/blob/master/inst/extdata/MethylationDataExample55.csv?raw=true",
-#'   age_when_measured = 44
+#'   methyl_data_input_path = "https://github.com/isglobal-brge/methylclock/blob/master/inst/extdata/MethylationDataExample55.csv?raw=true",
+#'   covariate_data_input_path = "https://raw.githubusercontent.com/isglobal-brge/methylclock/master/inst/extdata/SampleAnnotationExample55.csv"
 #' )
 #' }
 #'
 #' @export
-du.upload.methyl.clocks <- function(upload = TRUE, dict_name = "", action = du.enum.action()$ALL, data_input_path = "", data_version = "1_0", age_when_measured, database_name = "opal_data") {
+du.upload.methyl.clocks <- function(upload = TRUE, dict_name = "", action = du.enum.action()$ALL, methyl_data_input_path = "", covariates_data_input_path =, data_version = "1_0", age_when_measured, database_name = "opal_data") {
   du.check.package.version()
   du.check.session(upload)
 
@@ -76,20 +76,24 @@ du.upload.methyl.clocks <- function(upload = TRUE, dict_name = "", action = du.e
 #' @param data_input_path input path of the raw methylation data
 #' @param age_when_measured age of the child when the measurement took place (in months)
 #'
+#' @importFrom readr read_csv
+#' @importFrom tibble add_column
+#'
 #' @return the generated clocks with converted columns for child_id and the age_measured attached
 #'
 #' @noRd
-du.generate.methyl.data <- function(data_input_path, age_when_measured) {
+du.generate.methyl.data <- function(methyl_data_input_path, covariate_data_input_path) {
   requireNamespace("methylclock")
 
-  methyl_data <- read_csv(data_input_path)
+  methyl_data <- read_csv(methyl_data_input_path)
+  covariate_data <- read_csv(covariate_data_input_path)
 
-  data <- methylclock::DNAmAge(methyl_data)
+  data <- methylclock::DNAmAge(methyl_data, age = covariate_data$Age)
 
   colnames(data)[colnames(data) == "id"] <- "child_id"
 
-  data <- cbind(row_id = seq.int(nrow(data)), data)
-  data <- cbind(age = age_when_measured, data)
+  row_id = seq.int(nrow(data))
+  data <- add_column(data, row_id, before = 1)
 
   return(data)
 }
