46e56a92f75e5a93f71fb158b9132d5fe267dc40
Sido Haakma <sido@haakma.org>
2021-11-04 09:27:14 +0100

    fix: forgot quote

--- .azure/prepare.bash
+++ .azure/prepare.bash
@@ -5,5 +5,5 @@ echo "Create user libraries directory R [ ${R_LIBS_USER} ]"
 mkdir -p "${R_LIBS_USER}"
 
 Rscript -e "install.packages(c('git2r', 'covr', 'withr', 'devtools', 'lintr', 'mockery', 'pkgdown'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
-Rscript -e "install.packages(c(‘opalr’, ‘MolgenisArmadillo’, ‘maditr’), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')
+Rscript -e "install.packages(c('opalr', 'MolgenisArmadillo', 'maditr'), repos='https://cloud.r-project.org', lib='${R_LIBS_USER}')"
 cd "${BUILD_REPOSITORY_LOCALPATH}"
